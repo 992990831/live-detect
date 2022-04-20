5534f4bc050a4b5a678761be32893e9a89f0cc29
Andy-PoleStar <xiaohui.hu@polestar.com>
2022-04-20 09:05:52 +0800

    添加gettoken方法

--- src/Pages/VideoLive/index.tsx
+++ src/Pages/VideoLive/index.tsx
@@ -4,8 +4,7 @@ import { BrowserRouter as Router, Route, Link, Routes, useNavigate } from "react
 import { GetSessionCode, VideoVerify } from '../../Util/Util';
 import avator from '../../assets/images/avator.png';
 import './index.css';
-
-const token = '24.8b5d9f9d25257ac79bdc93b47b65f256.2592000.1650612168.282335-25828496';
+import axios from 'axios';
 
 export const VideoLive = () => {
     const camRef = useRef(null);
@@ -13,17 +12,63 @@ export const VideoLive = () => {
     const [sessionCode, setSessionCode] = useState('');
     const [actions, setActions] = useState('');
     const [btnDisable, setBtnDisable] = useState(false);
+
+    const [token, setToken] = useState<string>();
+
     useEffect(() => {
-        GetSessionCode(token).then((result) => {
-            if(result[0]==='XXX')
-            {
-                setBtnDisable(true);
+        const fn = async () => {
+            const tempToken = await GetToken();
+
+            if (!tempToken)
                 return;
-            }
-            setSessionCode(result[0]);
-            setActions(result[1]);
-        });
+
+            setToken(tempToken);
+
+            debugger;
+            GetSessionCode(tempToken).then((result) => {
+                if (result[0] === 'XXX') {
+                    setBtnDisable(true);
+                    return;
+                }
+                setSessionCode(result[0]);
+                setActions(result[1]);
+            });
+        };
+
+        fn();
+
     }, []);
+
+    const GetToken = async (): Promise<string> => {
+        // fetch('https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=R28VSZLeBFw1w8b3yrz67Ait&client_secret=Drzmlb7qNQfGoePWv2bptetZ2yzaRUGn', {
+        //     method: "Post",
+        //     mode:'no-cors',
+        //     headers: {
+        //         'Content-Type': 'text/plain'
+        //     }
+        // })
+
+        const token = await axios.get('http://106.75.216.135:8004/api/livedetect/token');
+
+        // const token = await fetch('https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=R28VSZLeBFw1w8b3yrz67Ait&client_secret=Drzmlb7qNQfGoePWv2bptetZ2yzaRUGn', {
+        //     method: "Post",
+        //     mode: 'no-cors',
+        //     headers: {
+        //         'Content-Type': 'text/plain'
+        //     }
+        // }).then(response => {
+        //     debugger;
+        //     return response.json();
+        // }).catch(error => {
+        //     debugger;
+        //     console.log(error);
+        //     alert('请检查是否余额不足');
+        //     return '';
+        // });
+
+        debugger;
+        return token.data.access_token;
+    }
 
     const navigate = useNavigate();
 
@@ -55,7 +100,7 @@ export const VideoLive = () => {
                 //视频的base64编码是不包含视频头的，如 data:video/mp4;base64,；
                 //setVideoStr(dataBase64.substring(45));
                 //VideoVerify(token, sessionCode, encodeURI(dataBase64.substring(45)));
-                VideoVerify(token, sessionCode, encodeURIComponent(dataBase64.split(',')[1]))
+                VideoVerify(token ?? '', sessionCode, encodeURIComponent(dataBase64.split(',')[1]))
                     .then((score) => {
                         if (score >= 0.75) {
                             navigate('/success');
@@ -73,7 +118,7 @@ export const VideoLive = () => {
     return (
         <>
             <p className='title'>请按要求录制视频</p>
-            <img src={avator} alt='avator' style={{width:'30vw', left:'35vw', position:'relative'}} />
+            <img src={avator} alt='avator' style={{ width: '30vw', left: '35vw', position: 'relative' }} />
             <p className='action-num'>{actions}</p>
             <ul className='notice'>
                 <li>环境安静，光线充足</li>
@@ -112,7 +157,7 @@ export const VideoLive = () => {
                 </div>
             </div> */}
 
-            <Space wrap block style={{ '--gap-vertical': '12px', position:'fixed', bottom:'30px', left:'15vw' }} align='center' justify='center' direction='vertical'>
+            <Space wrap block style={{ '--gap-vertical': '12px', position: 'fixed', bottom: '30px', left: '15vw' }} align='center' justify='center' direction='vertical'>
                 <Button disabled={btnDisable} shape='rounded' block color='primary' size='large' onClick={onCameraClick} style={{ width: '70vw' }}>
                     开始录制
                 </Button>
